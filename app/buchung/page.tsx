65d2272034d8738c013b50495a1088b42b744aef
Stephanie Tack <stephanietack@AirvonStephanie.fritz.box>
2026-03-27 20:50:56 +0100

    Update booking iframe styling

--- app/buchung/page.tsx
+++ app/buchung/page.tsx
@@ -6,7 +6,7 @@ export default function Buchung() {
         Pension Janina
       </h1> */}
 
-      <div className="w-full h-[2000px] overflow-hidden rounded-xl">
+      {/* <div className="w-full h-[2000px] overflow-hidden rounded-xl">
         <iframe
           src="https://beds24.com/booking2.php?propid=319626&hidefooter=yes&showsearch=0"
           width="100%"
@@ -14,7 +14,14 @@ export default function Buchung() {
           style={{ border: "none" }}
           scrolling="no"
         ></iframe>
-      </div>
+      </div> */}
+      <div className="w-full h-[2000px] overflow-hidden">
+  <iframe
+    src="https://beds24.com/booking2.php?propid=319626&hidefooter=yes&advancedays=2&referer=iframe&showsearch=0"
+    className="w-full h-full"
+    style={{ border: "none" }}
+  ></iframe>
+</div>
     </div>
   );
 }
