55a062e044507e96ce4a46f87650a06f999e11c1
Stephanie Tack <stephanietack@MacBook-Air-von-Stephanie.local>
2026-07-19 00:00:23 +0200

    update

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -3,37 +3,28 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request) {
-
   const { name, email, message } = await request.json();
 
   try {
-
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "Pension Janina <pension-janina@t-online.de>",
       to: process.env.CONTACT_EMAIL,
+      replyTo: email,
       subject: "Neue Kontaktanfrage",
       html: `
-        <h2>Neue Anfrage</h2>
-        <p><strong>Name:</strong> ${name}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p>${message}</p>
-      `,
+    <h2>Neue Anfrage</h2>
+    <p><strong>Name:</strong> ${name}</p>
+    <p><strong>E-Mail:</strong> ${email}</p>
+    <p>${message}</p>
+  `,
     });
-
 
     return Response.json({
-      success: true
+      success: true,
     });
-
-
-  } catch(error) {
-
+  } catch (error) {
     console.error(error);
 
-    return Response.json(
-      {success:false},
-      {status:500}
-    );
-
+    return Response.json({ success: false }, { status: 500 });
   }
 }
